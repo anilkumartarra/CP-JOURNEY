72af9f22fc03c8102d8c6b85809fda35cb4b115d
Tarra Anil Kumar <91973092+anilkumartarra@users.noreply.github.com>
2021-12-04 23:30:40 +0530

    Update BINARY_TREE.cpp

--- TREES/BINARY_TREE.cpp
+++ TREES/BINARY_TREE.cpp
@@ -44,8 +44,8 @@ void preorder(node *root)
     else
     {
         cout<<root->data<<" ";
-        inorder(root->left);
-        inorder(root->right);
+        preorder(root->left);
+        preorder(root->right);
     }
 }
 void postorder(node *root)
@@ -56,8 +56,8 @@ void postorder(node *root)
     }
     else
     {
-        inorder(root->left);
-        inorder(root->right);
+        postorder(root->left);
+        postorder(root->right);
         cout<<root->data<<" ";
     }
 }
